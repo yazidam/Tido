f0da6352ed7de4d6f4ef83cbd07815db092f812b
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2024-02-04 16:35:48 +0100

    adding total

--- src/components/CounterList/index.js
+++ src/components/CounterList/index.js
@@ -1,54 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import Counter from "../Counter";
 
-function CounterList() {
-  const initArrayOfObjects = [
-    { id: 1, title: "Title 1", value: 1 },
-    { id: 2, title: "Title 2", value: 20 },
-    { id: 3, title: "Title 3", value: 25 },
-  ];
-
-  const [arrayOfObjects, setArrayOfObjects] = useState(initArrayOfObjects);
-
-  const increment = (id) => {
-    let items = [...arrayOfObjects];
-    let i = items.findIndex((elem) => elem.id === id);
-    let counter = items[i];
-    if (counter) {
-      counter = { ...counter, value: counter.value + 1 };
-
-      items[i] = counter;
-    }
-    setArrayOfObjects(items);
-  };
-  const decrement = (id) => {
-    let items = [...arrayOfObjects];
-    let i = items.findIndex((elem) => elem.id === id);
-    let counter = items[i];
-
-    if (counter) {
-      counter = { ...counter, value: counter.value - 1 };
-
-      items[i] = counter;
-    }
-    setArrayOfObjects(items);
-  };
-  const reset = (id) => {
-    let items = [...arrayOfObjects];
-    let i = items.findIndex((elem) => elem.id === id);
-    let counter = items[i];
-
-    if (counter) {
-      counter = { ...counter, value: 0 };
-
-      items[i] = counter;
-    }
-    setArrayOfObjects(items);
-  };
-
+function CounterList({ arrayOfObjects, decrement, increment, reset }) {
   return (
     <div>
-      {arrayOfObjects.map((elem) => (
+      {arrayOfObjects?.map((elem) => (
         <Counter
           key={elem.id}
           id={elem.id}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,66 @@
-import React from "react";
+/* eslint-disable no-unreachable */
+import React, { useState } from "react";
+import CounterList from "../CounterList";
 
 function Header() {
-  return <div>summ : 0</div>;
+  const initArrayOfObjects = [
+    { id: 1, title: "Title 1", value: 1 },
+    { id: 2, title: "Title 2", value: 20 },
+    { id: 3, title: "Title 3", value: 25 },
+  ];
+
+  const [arrayOfObjects, setArrayOfObjects] = useState(initArrayOfObjects);
+  const total = arrayOfObjects.reduce((acc, item) => acc + item.value, 0);
+
+  const increment = (id) => {
+    let items = [...arrayOfObjects];
+    let i = items.findIndex((elem) => elem.id === id);
+    let counter = items[i];
+    if (counter) {
+      counter = { ...counter, value: counter.value + 1 };
+
+      items[i] = counter;
+    }
+    setArrayOfObjects(items);
+  };
+
+  const decrement = (id) => {
+    let items = [...arrayOfObjects];
+    let i = items.findIndex((elem) => elem.id === id);
+    let counter = items[i];
+
+    if (counter) {
+      counter = { ...counter, value: counter.value - 1 };
+
+      items[i] = counter;
+    }
+    setArrayOfObjects(items);
+  };
+
+  const reset = (id) => {
+    let items = [...arrayOfObjects];
+    let i = items.findIndex((elem) => elem.id === id);
+    let counter = items[i];
+
+    if (counter) {
+      counter = { ...counter, value: 0 };
+
+      items[i] = counter;
+    }
+    setArrayOfObjects(items);
+  };
+
+  return (
+    <>
+      <div>summ : {total}</div>
+      <CounterList
+        arrayOfObjects={arrayOfObjects}
+        decrement={decrement}
+        increment={increment}
+        reset={reset}
+      />
+    </>
+  );
 }
 
 export default Header;
